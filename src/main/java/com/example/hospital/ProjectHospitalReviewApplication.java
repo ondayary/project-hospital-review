2543cbc783078181cabeb231c3cc87a910d9a414
ondayary@gmail.com <ondayary@gmail.com>
2022-12-08 14:41:35 +0900

    gitlab 연동을 위한 project build

---
+++ src/main/java/com/example/hospital/ProjectHospitalReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHospitalReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectHospitalReviewApplication.class, args);
+    }
+
+}
